647d7725230076291be7e34af027bc143863e611
maxchehab <maxchehab@gmail.com>
2017-12-03 20:23:45 -0800

    Normalized GoogleCivic structures

--- src/structures.go
+++ src/structures.go
@@ -16,48 +16,10 @@ type RequestData struct {
 
 //GoogleCivic structure
 type GoogleCivic struct {
-	Kind            string `json:"kind"`
-	NormalizedInput struct {
-		Line1 string `json:"line1"`
-		City  string `json:"city"`
-		State string `json:"state"`
-		Zip   string `json:"zip"`
-	} `json:"normalizedInput"`
-	Divisions struct {
-		OcdDivisionCountryUs struct {
-			Name          string `json:"name"`
-			OfficeIndices []int  `json:"officeIndices"`
-		} `json:"ocd-division/country:us"`
-		OcdDivisionCountryUsStateOr struct {
-			Name          string `json:"name"`
-			OfficeIndices []int  `json:"officeIndices"`
-		} `json:"ocd-division/country:us/state:or"`
-		OcdDivisionCountryUsStateOrCd2 struct {
-			Name          string `json:"name"`
-			OfficeIndices []int  `json:"officeIndices"`
-		} `json:"ocd-division/country:us/state:or/cd:2"`
-		OcdDivisionCountryUsStateOrCircuitCourt11 struct {
-			Name string `json:"name"`
-		} `json:"ocd-division/country:us/state:or/circuit_court:11"`
-		OcdDivisionCountryUsStateOrCountyDeschutes struct {
-			Name          string `json:"name"`
-			OfficeIndices []int  `json:"officeIndices"`
-		} `json:"ocd-division/country:us/state:or/county:deschutes"`
-		OcdDivisionCountryUsStateOrSldl54 struct {
-			Name          string `json:"name"`
-			OfficeIndices []int  `json:"officeIndices"`
-		} `json:"ocd-division/country:us/state:or/sldl:54"`
-		OcdDivisionCountryUsStateOrSldu27 struct {
-			Name          string `json:"name"`
-			OfficeIndices []int  `json:"officeIndices"`
-		} `json:"ocd-division/country:us/state:or/sldu:27"`
-	} `json:"divisions"`
+	Kind    string `json:"kind"`
 	Offices []struct {
-		Name            string   `json:"name"`
-		DivisionID      string   `json:"divisionId"`
-		Levels          []string `json:"levels,omitempty"`
-		Roles           []string `json:"roles,omitempty"`
-		OfficialIndices []int    `json:"officialIndices"`
+		Name            string `json:"name"`
+		OfficialIndices []int  `json:"officialIndices"`
 	} `json:"offices"`
 	Officials []struct {
 		Name    string `json:"name"`
@@ -68,15 +30,6 @@ type GoogleCivic struct {
 			State string `json:"state"`
 			Zip   string `json:"zip"`
 		} `json:"address"`
-		Party    string   `json:"party"`
-		Phones   []string `json:"phones"`
-		Urls     []string `json:"urls,omitempty"`
-		PhotoURL string   `json:"photoUrl,omitempty"`
-		Channels []struct {
-			Type string `json:"type"`
-			ID   string `json:"id"`
-		} `json:"channels,omitempty"`
-		Emails []string `json:"emails,omitempty"`
 	} `json:"officials"`
 }
 
